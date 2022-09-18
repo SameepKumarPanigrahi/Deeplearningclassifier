889338c3ea952066d2c9a54829cc4c9fb20a50ae
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-09-18 18:00:09 +0530

    Prepare Base model Stage done

--- src/deepClassifier/pipeline/stg_02-prepare_base_model.py
+++ src/deepClassifier/pipeline/stg_02-prepare_base_model.py
@@ -1,16 +1,16 @@
 from deepClassifier.config import ConfigurationManager
-from deepClassifier.components import DataIngestion
 from deepClassifier import logger
+from deepClassifier.components import PrepareBaseModel
 
+STAGE_NAME = "Prepare base model"
 
-STAGE_NAME = "Data Ingestion Stage"
 def main():
     config = ConfigurationManager()
-    data_ingestion_config = config.get_data_ingestion_config()
-    data_ingestion = DataIngestion(config=data_ingestion_config)
-    data_ingestion.download_file()
-    data_ingestion.unzip_and_clean()
-    
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+
     
 if __name__ == '__main__':
     try:
