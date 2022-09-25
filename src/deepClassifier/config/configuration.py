70891804f8c7f4151768fd19cfc5c74017af658a
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-09-25 20:02:19 +0530

    Model Evaluation research

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -5,6 +5,7 @@ from deepClassifier.entity.config_entity import (
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
     TrainingConfig,
+    EvaluationConfig,
 )
 import os
 
@@ -77,3 +78,12 @@ class ConfigurationManager:
             params_image_size=params.IMAGE_SIZE,
         )
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=self.config.training.trained_model_path,
+            training_data=Path(os.path.join(self.config.data_ingestion.unzip_dir, "PetImages")),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
---
+++ src/deepClassifier/pipeline/stage_04_evaluation_stage.py
@@ -0,0 +1,25 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier import logger
+from deepClassifier.components.evaluation import Evaluation
+
+STAGE_NAME = "Evaluation stage"
+
+
+def main():
+    config = ConfigurationManager()
+    validation_config = config.get_validation_config()
+    evaluation = Evaluation(config=validation_config)
+    evaluation.evaluation()
+    evaluation.save_score()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        main()
+        logger.info(
+            f">>>>> stage {STAGE_NAME} completed successfully <<<<< \n \n X{'=='*60}X"
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise e
