d488ab1ba53188c34cdface025a9713b86e9905b
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-10-11 23:25:04 +0530

    Adding Logger

--- src/deepClassifier/components/best_model_selector.py
+++ src/deepClassifier/components/best_model_selector.py
@@ -5,7 +5,7 @@ from deepClassifier.config.configuration import BestModelSelectorConfig
 from box import ConfigBox
 from deepClassifier.utils.common import *
 import os
-
+from deepClassifier import logging
 
 class BestModelSelector:
     def __init__(self, config: BestModelSelectorConfig):
@@ -38,21 +38,26 @@ class BestModelSelector:
     
     def evaluation(self):
         self.exisitng_model_score = None
+        logging.info("If the model is previously exist in the prediction service then load the model an calculate the scores")
         if(os.path.exists(self.config.existing_model_path)):
             self.model = self.load_model(self.config.existing_model_path)
             self._valid_generator()
             self.score = self.model.evaluate(self.valid_generator)
             scores = {"loss": self.score, "accuracy": self.score[1]}
             self.exisitng_model_score = ConfigBox(scores)
+        logging.info("Load the scores of currently produced model from scores.json file")
         self.new_model_score = load_json(self.config.score_file_path)
         
     def select_best_model(self):
         #newly_trained_model_directory = os.path.dirname(self.config.trained_model_path)
         final_destination = os.path.dirname(self.config.existing_model_path)
         if (self.exisitng_model_score == None):
+            logging.info("There is no model exisiting previosuly so copying this model to the prediction service")
             shutil.copy(self.config.trained_model_path, final_destination)
         else:
+            logging.info("There is a model exist previousle so compare the scores")
             if(self.new_model_score.accuracy > self.exisitng_model_score.accuracy):
+                logging.info("Newly created model's score is greater then the previous so replacing the model in prediction service")
                 os.remove(self.config.existing_model_path)
                 shutil.copy(self.config.trained_model_path, final_destination)
             
