40ec6c885ca6a24428eaf5f96d49f687962b7cca
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-10-15 18:25:59 +0530

    spacing

--- tests/unit/test_callback_and_training.py
+++ tests/unit/test_callback_and_training.py
@@ -15,6 +15,7 @@ class Test_Train_get_base_model:
         params_is_augmentation=True,
         params_image_size=[224, 224, 3]
     )
+    
     def test_train_getbase_model(self):
         training = Training(config=self.training_config)
         training.get_base_model()
@@ -33,6 +34,7 @@ class Test_Train_valid_generator:
         params_is_augmentation=True,
         params_image_size=[224, 224, 3]
     )
+    
     def test_train_valid_generator(self):
         training = Training(config=self.training_config)
         training.train_valid_generator()
@@ -45,6 +47,7 @@ class Test_Train_method_train:
         tensorboard_root_log_dir="tests/data/prepare_callbacks/tensorboard_log_dir",
         checkpoint_model_filepath="tests/data/prepare_callbacks/checkpoint_dir/model.h5"
     )
+    
     training_config = TrainingConfig(
         root_dir="tests/data/training",
         trained_model_path="tests/data/training/model.h5",
@@ -55,6 +58,7 @@ class Test_Train_method_train:
         params_is_augmentation=True,
         params_image_size=[224, 224, 3]
     )
+    
     def test_train(self):
         #model_ckpt_dir = os.path.dirname(self.prepare_call_back_config.checkpoint_model_filepath)
         # create_directories([model_ckpt_dir, self.prepare_call_back_config.tensorboard_root_log_dir])
